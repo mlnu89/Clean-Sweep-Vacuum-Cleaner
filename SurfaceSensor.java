b6ced32e3911a89a5696970f6f43ace2491c6ed1
Ayaz Mohammed <mlnu89@depaul.edu>
2024-10-13 14:21:55 -0500

    Stubbed SurfaceSensor class to detect surface types.

--- SurfaceSensor.java
+++ SurfaceSensor.java
@@ -1,5 +1,5 @@
 public class SurfaceSensor {
-	// Always returns bare floor; can be expanded to detect different surface types
+	// Always returns bare floor; can be expanded to detect different surface types.
 	public SurfaceType getSurfaceType(int x, int y) {
 		return SurfaceType.BARE_FLOOR;
 	}
